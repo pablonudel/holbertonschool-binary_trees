63ca017ec92e0171cd762606575c1c6dce4c3242
pablonudel <pablonudel@gmail.com>
2024-12-09 18:56:53 +0100

    adding conditional to check if a new_node is created

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -15,6 +15,8 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 		return (NULL);
 
 	new_node = binary_tree_node(parent, value);
+	if (!new_node)
+		return (NULL);
 
 	if (parent->right != NULL)
 	{
