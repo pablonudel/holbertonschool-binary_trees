2b1e33ea111ff913187b299ec2ad1827dc9b476c
pablonudel <pablonudel@gmail.com>
2024-12-09 18:51:02 +0100

    fixing trailing spaces and long lines

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -17,7 +17,7 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 	new_node = binary_tree_node(parent, value);
 
 	if (parent->right != NULL)
-	{	
+	{
 		new_node->right = parent->right;
 		parent->right->parent = new_node;
 	}
