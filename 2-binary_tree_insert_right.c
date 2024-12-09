e0f62edf81930a603af330d337681ad1248fd8d7
pablonudel <pablonudel@gmail.com>
2024-12-09 15:36:02 +0100

    adding the call to binary_tree_node function and fixing node assigments

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -14,17 +14,12 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 	if (!parent || !value)
 		return (NULL);
 
-	new_node = malloc(sizeof(binary_tree_t));
-	if (!new_node)
-		return (NULL);
+	new_node = binary_tree_node(parent, value);
 
-	new_node->n = value;
-
-	if (parent->right)
-	{
-		parent->right->right = parent->right;
-		parent->right = new_node;
-		return (new_node);
+	if (parent->right != NULL)
+	{	
+		new_node->right = parent->right;
+		parent->right->parent = new_node;
 	}
 
 	parent->right = new_node;
