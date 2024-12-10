6e3631528ddc4ace21208e7f76764ecbfbc740cc
azbin-spec <dydouhabdellah82@gmail.com>
2024-12-10 14:56:43 +0100

    9-binary_tree_height.c , correction des erreurs

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,27 +1,20 @@
 #include "binary_trees.h"
-
 /**
- * binary_tree_height - a function that measures the height of a binary tree
- * @tree: tree to go through
- * Return: the height
- */
-
+ 
+binary_tree_height - measures the height of a binary tree*
+@tree: a pointer to the root node of the tree to measure the height*
+Return: size_t - the height of tree*/
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t l = 0;
-	size_t r = 0;
+        size_t l = 0, r = 0;
 
-	if (tree == NULL)
-	{
-		return (0);
-	}
-	else
-	{
-		if (tree)
-		{
-			l = tree->left ? 1 + binary_tree_height(tree-left) : 0;
-			r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
-		}
-		return ((1 > r) ? 1 : r);
-	}
+        if (!tree)
+                return (0);
+
+        if (tree->left)
+                l = 1 + binary_tree_height(tree->left);
+        if (tree->right)
+                r = 1 + binary_tree_height(tree->right);
+
+        return (l > r ? l : r);
 }
