01dd29b88aaadc4dbf8fdc60acc2cd9338163918
pablonudel <pablonudel@gmail.com>
2024-12-10 18:53:03 +0100

    simplifying sintaxis

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -8,16 +8,11 @@
  */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-	size_t leaves = 0;
-
 	if (!tree)
 		return (0);
 
 	if (!tree->left && !tree->right)
-		leaves += 1;
+		return (1);
 
-	leaves += binary_tree_leaves(tree->left);
-	leaves += binary_tree_leaves(tree->right);
-
-	return (leaves);
+	return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
 }
