0cd34f00a87f9b3979396328dbb4988634a032b7
pablonudel <pablonudel@gmail.com>
2024-12-10 15:54:28 +0100

    simplifying sintaxis

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -8,16 +8,8 @@
  */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-	size_t size = 1;
-
 	if (!tree)
 		return (0);
 
-	if (tree->left)
-		size += binary_tree_size(tree->left);
-
-	if (tree->right)
-		size += binary_tree_size(tree->right);
-
-	return (size);
+	return (1 + binary_tree_size(tree->left) + binary_tree_size(tree->right));
 }
