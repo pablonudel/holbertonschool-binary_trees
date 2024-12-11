b02cf3724422e52416bed76bd51e238e2d0fa994
azbin-spec <dydouhabdellah82@gmail.com>
2024-12-11 14:37:44 +0100

    6-binary_tree_preorder.c

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -9,13 +9,10 @@
 
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
-	if (!tree)
-		return;
-
-	if (!func)
+	if (!tree || !func)
 		return;
 
 	func(tree->n);
 	binary_tree_preorder(tree->left, func);
-	binary_tree_preorder(tree->right, func);
+	binary_tree_preorder(tree->right, func); 
 }	
