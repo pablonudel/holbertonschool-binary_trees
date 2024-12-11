78c6a67b732226e506065f60955b772edfe16876
azbin-spec <dydouhabdellah82@gmail.com>
2024-12-11 09:30:16 +0100

    fixed description error

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -2,8 +2,9 @@
 
 /**
  * binary_tree_preorder - Goes through a binary tree using pre-order traversal.
- * @tree: A pointer to the root node of the tree to traverse.
+ * @tree: A pointer to the root node of the tree to traverse. 
  * @func: A pointer to a function to call for each node.
+ * Return: No Return value 
  */
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
