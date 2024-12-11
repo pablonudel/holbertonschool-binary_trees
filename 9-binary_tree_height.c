f06550f8331451a6ca3f0ab3a37cc826446f0106
azbin-spec <dydouhabdellah82@gmail.com>
2024-12-11 09:20:29 +0100

    corrected a description error

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -3,7 +3,8 @@
  
 binary_tree_height - measures the height of a binary tree*
 @tree: a pointer to the root node of the tree to measure the height*
-Return: size_t - the height of tree*/
+Return: size_t - the height of tree
+*/
 size_t binary_tree_height(const binary_tree_t *tree)
 {
         size_t l = 0, r = 0;
