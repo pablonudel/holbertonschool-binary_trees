f01208c13034a95c565e52197de87a376e125a5a
azbin-spec <dydouhabdellah82@gmail.com>
2024-12-11 14:28:13 +0100

    6-binary_tree_preorder.c

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -2,9 +2,8 @@
 
 /**
  * binary_tree_preorder - Goes through a binary tree using pre-order traversal.
- * @tree: A pointer to the root node of the tree to traverse. 
+ * @tree: A pointer to the root node of the tree to traverse.
  * @func: A pointer to a function to call for each node.
- * Return: No Return value 
  */
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
@@ -14,4 +13,4 @@ void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 		binary_tree_preorder(tree->left, func);
 		binary_tree_preorder(tree->right, func);
 	}
-}		
+}	
