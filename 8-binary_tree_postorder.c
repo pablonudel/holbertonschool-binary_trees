ff07cdce9d75814e2141e3a499b37c391d30ce08
pablonudel <pablonudel@gmail.com>
2024-12-11 08:37:42 +0100

    checking description

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -2,7 +2,6 @@
 /**
  * binary_tree_postorder - goes through a binary tree
  * using post-order traversal
- *
  * @tree: a pointer to the root node of the tree to traverse
  * @func: a pointer to a function to call for each node
  *
